5dd2c5a0c9291c7ef08a5351dcec43fd2eed6093
Lucas Simon <lucassrod@gmail.com>
2013-12-10 21:49:22 -0200

    Insere o sitemaps as urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -3,6 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+sitemaps = {
+    # Place sitemaps here
+}
+
+
 urlpatterns = patterns(
     '',
     # Examples:
@@ -10,4 +15,12 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # SEO API's
+    url(
+        r'^sitemap.xml$',
+        'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': sitemaps}
+    ),
+
 )
