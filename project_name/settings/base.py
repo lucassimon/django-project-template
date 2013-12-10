7c6addc0e13ec642f0b0b88b7b88a7875ab2ba63
Lucas Simon <lucassrod@gmail.com>
2013-12-10 21:30:50 -0200

    First commit

---
+++ project_name/settings/base.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from unipath import Path
+PROJECT_DIR = Path(__file__).ancestor(3)
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = []
+
+ADMINS = (
+    ('Lucas Simon Rodrigues Magalhaes', 'lucassrod@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+LANGUAGES = (
+    ('pt-br', u'Portugues'),
+    ('en', u'Ingles'),
+)
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+LANGUAGE_CODE = 'pt-br'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+MEDIA_ROOT = PROJECT_DIR.child('media')
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = PROJECT_DIR.child('public')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child("assets"),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = '{{ secret_key|safe }}'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = '{{ project_name }}.urls'
+
+WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child("templates"),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    # my apps
+    'core',
+
+    # third apps
+    'south',
+)
+
+ALLOWED_HOSTS = []
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
---
+++ project_name/settings/staging.py
@@ -0,0 +1,30 @@
+from base import *
+from decouple import Config
+import dj_database_url
+
+
+config = Config(PROJECT_DIR.child('confs')+'/settings.ini')
+
+########## TEST SETTINGS
+TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
+TEST_DISCOVER_TOP_LEVEL = PROJECT_DIR
+TEST_DISCOVER_ROOT = PROJECT_DIR
+TEST_DISCOVER_PATTERN = "test_*"
+########## END TEST SETTINGS
+
+########## DEBUG CONFIGURATION
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+COMPRESS_ENABLED = not DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## DATABASE CONFIGURATION
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_TEST_URL')
+    )
+}
+########## END DATABASE CONFIGURATION
