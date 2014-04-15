c1f58fd026d5c8c0fd54299350b3146dd11036a4
Lucas Simon <lucassrod@gmail.com>
2014-04-15 15:20:15 -0300

    Corrige o fabric

--- fabfile.py
+++ fabfile.py
@@ -1,210 +1,166 @@
+# -*- coding:utf-8 -*-
+
 """
 Starter fabfile for deploying the {{ project_name }} project.
 
 Change all the things marked CHANGEME. Other things can be left at their
 defaults if you are happy with the default layout.
+
 """
 
-import posixpath
-
-from fabric.api import run, local, env, settings, cd, task
-from fabric.contrib.files import exists
-from fabric.operations import _prefix_commands, _prefix_env_vars
+from fabric.api import run, env,  cd, task, sudo
+from fabric.colors import green, red
 #from fabric.decorators import runs_once
-#from fabric.context_managers import cd, lcd, settings, hide
-
+from fabric.context_managers import shell_env
 # CHANGEME
-env.hosts = ['user@{{ project_name }}.example.com']
-env.code_dir = '/srv/www/{{ project_name }}'
-env.project_dir = '/srv/www/{{ project_name }}/{{ project_name }}'
-env.static_root = '/srv/www/{{ project_name }}/static/'
-env.virtualenv = '/srv/www/{{ project_name }}/.virtualenv'
-env.code_repo = 'git@github.com:user/{{project_name}}.git'
-env.django_settings_module = '{{ project_name }}.settings'
 
-# Python version
-PYTHON_BIN = "python2.7"
-PYTHON_PREFIX = ""  # e.g. /usr/local  Use "" for automatic
-PYTHON_FULL_PATH = "%s/bin/%s" % (PYTHON_PREFIX, PYTHON_BIN) if PYTHON_PREFIX else PYTHON_BIN
+if not hasattr(env, 'server'):
+    print(green('Server de Produção'))
+    env.server = 'production'
+    env.hosts = ['{{project}}@{{ip}}']
+    env.project_name = u'{{project_name}}'
+    env.shell = u'/bin/zsh -c'
+    env.code_dir = u'~/sites/{{project_name}}'
+    env.git_repo = u'git@github.com:lucassimon/{{project_repo}}git'
+    env.settings = u'--settings={{project_name}}.settings.production'
+    env.virtualenv = '~/venvs/{{project_repo}}/'
+    env.python_bin = env.virtualenv + 'bin/python'
+    env.pip_bin = env.virtualenv + 'bin/pip'
+    env.webserver = u'nginx'
+elif env.server == 'staging':
+    print(green('Server de Staging'))
+    env.server = 'staging'
 
-# Set to true if you can restart your webserver (via wsgi.py), false to stop/start your webserver
-# CHANGEME
-DJANGO_SERVER_RESTART = False
 
-
-def virtualenv(venv_dir):
+def print_env_and_user():
     """
-    Context manager that establishes a virtualenv to use.
+    Print the envirioment and user
     """
-    return settings(venv=venv_dir)
+    print(red("Executing on %s as %s" % (env.host, env.user)))
 
 
-def run_venv(command, **kwargs):
+def django_manage(
+    command='help',
+    virtualenv='pywatch.com.br',
+    settings=env.settings
+):
     """
-    Runs a command in a virtualenv (which has been specified using
-    the virtualenv context manager
+    Returns a absolute path to execute manage.py
     """
-    run("source %s/bin/activate" % env.virtualenv + " && " + command, **kwargs)
+    return "/bin/bash -l -c 'source /usr/local/bin/virtualenvwrapper.sh && workon " + virtualenv + " && " + env.python_bin + " " + env.code_dir + "/manage.py " + command + " " + settings + "'"
 
 
-def install_dependencies():
-    ensure_virtualenv()
-    with virtualenv(env.virtualenv):
-        with cd(env.code_dir):
-            run_venv("pip install -r {{ project_name }}/requirements/production.txt")
-
-
-def ensure_virtualenv():
-    if exists(env.virtualenv):
-        return
-
+def git(cmd):
+    """
+    Create a method to execute git on the server
+    """
     with cd(env.code_dir):
-        run("virtualenv --no-site-packages --python=%s %s" %
-            (PYTHON_BIN, env.virtualenv))
-        run("echo %s > %s/lib/%s/site-packages/projectsource.pth" %
-            (env.project_dir, env.virtualenv, PYTHON_BIN))
+        run("git %s" % cmd)
 
 
-def ensure_src_dir():
-    if not exists(env.code_dir):
-        run("mkdir -p %s" % env.code_dir)
-    with cd(env.code_dir):
-        if not exists(posixpath.join(env.code_dir, '.git')):
-            run('git clone %s .' % (env.code_repo))
-
-
-def push_sources():
+def checkout_master():
     """
-    Push source code to server
+    Run git checkout master command on repository
     """
-    ensure_src_dir()
-    local('git push origin master')
+    git("checkout master")
+
+
+def pull():
+    """
+    Run git pull command on repository
+    """
+    git("pull --rebase")
+
+
+def restart():
+    """
+    Restart supervisor and webserver
+    """
+    sudo("supervisorctl restart pywatch:%s_pywatch_com_br" % (env.server))
+    sudo("service %s restart" % env.webserver)
+
+
+def start():
+    """
+    Start supervisor and webserver
+    """
+    sudo("supervisorctl restart pywatch:%s_pywatch_com_br" % (env.server))
+    sudo("service %s restart" % env.webserver)
+
+
+def stop():
+    """
+    Stopping supervisor
+    """
+    sudo("supervisorctl restart pywatch:%s_pywatch_com_br" % (env.server))
+
+
+def install_requirements():
+    """
+    Install all requirements by the server
+    """
     with cd(env.code_dir):
-        run('git pull origin master')
+        run(
+            "%s install -r %s/requirements/%s.txt"
+            % (
+                env.pip_bin,
+                env.code_dir,
+                env.server
+            )
+        )
 
 
-@task
-def run_tests():
-    """ Runs the Django test suite as is.  """
-    local("./manage.py test")
-
-
-@task
-def version():
-    """ Show last commit to the deployed repo. """
-    with cd(env.code_dir):
-        run('git log -1')
+def collectstatic():
+    """
+    Collect the static files
+    """
+    with shell_env(WORKON_HOME='~/venvs'):
+        run(django_manage(command='collectstatic -l --noinput'))
 
 
 @task
 def uname():
     """ Prints information about the host. """
+    print_env_and_user()
     run("uname -a")
 
 
 @task
-def webserver_stop():
-    """
-    Stop the webserver that is running the Django instance
-    """
-    run("service apache2 stop")
-
-
-@task
-def webserver_start():
-    """
-    Starts the webserver that is running the Django instance
-    """
-    run("service apache2 start")
-
-
-@task
-def webserver_restart():
-    """
-    Restarts the webserver that is running the Django instance
-    """
-    if DJANGO_SERVER_RESTART:
-        with cd(env.code_dir):
-            run("touch %s/wsgi.py" % env.project_dir)
-    else:
-        with settings(warn_only=True):
-            webserver_stop()
-        webserver_start()
-
-
-def restart():
-    """ Restart the wsgi process """
-    with cd(env.code_dir):
-        run("touch %s/{{ project_name }}/wsgi.py" % env.code_dir)
-
-
-def build_static():
-    assert env.static_root.strip() != '' and env.static_root.strip() != '/'
-    with virtualenv(env.virtualenv):
-        with cd(env.code_dir):
-            run_venv("./manage.py collectstatic -v 0 --clear --noinput")
-
-    run("chmod -R ugo+r %s" % env.static_root)
-
-
-@task
-def first_deployment_mode():
-    """
-    Use before first deployment to switch on fake south migrations.
-    """
-    env.initial_deploy = True
-
-
-@task
-def update_database(app=None):
-    """
-    Update the database (run the migrations)
-    Usage: fab update_database:app_name
-    """
-    with virtualenv(env.virtualenv):
-        with cd(env.code_dir):
-            if getattr(env, 'initial_deploy', False):
-                run_venv("./manage.py syncdb --all")
-                run_venv("./manage.py migrate --fake --noinput")
-            else:
-                run_venv("./manage.py syncdb --noinput")
-                if app:
-                    run_venv("./manage.py migrate %s --noinput" % app)
-                else:
-                    run_venv("./manage.py migrate --noinput")
-
-
-@task
-def sshagent_run(cmd):
-    """
-    Helper function.
-    Runs a command with SSH agent forwarding enabled.
-
-    Note:: Fabric (and paramiko) can't forward your SSH agent.
-    This helper uses your system's ssh to do so.
-    """
-    # Handle context manager modifications
-    wrapped_cmd = _prefix_commands(_prefix_env_vars(cmd), 'remote')
-    try:
-        host, port = env.host_string.split(':')
-        return local(
-            "ssh -p %s -A %s@%s '%s'" % (port, env.user, host, wrapped_cmd)
-        )
-    except ValueError:
-        return local(
-            "ssh -A %s@%s '%s'" % (env.user, env.host_string, wrapped_cmd)
-        )
-
-
-@task
 def deploy():
-    """
-    Deploy the project.
-    """
-    with settings(warn_only=True):
-        webserver_stop()
-    push_sources()
-    install_dependencies()
-    update_database()
-    build_static()
-    webserver_start()
+    """ Deploy the application """
+    print_env_and_user()
+    pull()
+    install_requirements()
+    with shell_env(WORKON_HOME='~/venvs'):
+        run(django_manage(command='syncdb --noinput'))
+    with shell_env(WORKON_HOME='~/venvs'):
+        run(django_manage(command='migrate --all'))
+    collectstatic()
+    restart()
+
+
+@task
+def haystack_update_index():
+    """Haystack command to update indexes"""
+    print_env_and_user()
+    with shell_env(WORKON_HOME='~/venvs'):
+        run(django_manage(command='update_index'))
+
+
+@task
+def haystack_rebuild_index():
+    """Haystack command to rebuild indexes"""
+    print_env_and_user()
+    with shell_env(WORKON_HOME='~/venvs'):
+        run(django_manage(command='rebuild_index'))
+
+
+@task
+def run_manage():
+    """ Run manage.py with command line """
+    print_env_and_user()
+    command_line = raw_input(
+        'Digite o comando para o manage.py \n Ex: help, migrate, dbshell\n> '
+    )
+    with shell_env(WORKON_HOME='~/venvs'):
+        run(django_manage(command=command_line))
