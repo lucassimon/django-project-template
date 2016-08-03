680dbf566bc34dd5ba7408365f0b32af16946d95
Lucas Simon <lucas.simon@2xt.com.br>
2016-08-03 18:35:01 -0300

    Corrige url de sitemap e djangodebugtoolbar

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -12,14 +13,20 @@ sitemaps = {
 
 
 urlpatterns = [
-    '',
     url(r'^admin/', include(admin.site.urls)),
 
     # SEO API's
     url(
-        r'^sitemap.xml$',
-        'django.contrib.sitemaps.views.sitemap',
-        {'sitemaps': sitemaps}
+        r'^sitemap\.xml$',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
     ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
