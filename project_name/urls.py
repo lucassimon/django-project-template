0a8a8a86736949f60198c6d625abf33d9e0b8aa9
Lucas Simon <lucas.simon@2xt.com.br>
2016-08-11 14:21:05 -0300

    Insere a renderizacao de staticos quando o debug esta ligado

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib.sitemaps.views import sitemap
-from django.conf.urls.static import static
 from django.conf import settings
 
 
@@ -23,10 +22,20 @@ urlpatterns = [
         name='django.contrib.sitemaps.views.sitemap'
     ),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     import debug_toolbar
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
